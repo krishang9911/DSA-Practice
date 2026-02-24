f1f73c5cd1b4714b8a437c9cbdef6e1cfebf4b94
Krishang <krishangmahajan9911@gmail.com>
2026-02-24 23:49:43 +0530

    updated non_primtive_ds.cpp

--- non_primitive_ds.cpp
+++ non_primitive_ds.cpp
@@ -5,7 +5,7 @@
 //   Represent relationship between different data elements
 //   Size can be changed dynamically
 
-// TYPES OF PRIMTIVE DATA STRUCTURES
+// TYPES OF NON PRIMTIVE DATA STRUCTURES
 // 1) Linear DS
 //    Elements are arranged sequentially
 //    So, each element(except the first and last) has a
@@ -26,3 +26,55 @@
 //       does not exist sometimes
 
 int arr[5] = {1, 2, 3, 4, 5};
+
+// b) Linked List
+//   Collection of nodes 
+//   Each node consists of 
+//    a) data     and    b) pointer that points to the address of
+//                            next node
+
+// Advantages
+//  Dynamic size
+//  Insertion/deletion is not costly as
+//   no shifting is required due to absence of indexing
+//  Can be created using small scattered pieces of memory
+
+//  Disadvantages
+//   No direct access(as there is no indexing)
+//   Extra space is required for pointer
+//   Homogenous data storage
+
+//  Stack
+//  A linear data structure that follows LIFO(Last in 
+//   First Out) principle.
+//  Like a physical stack of plates
+//  Elements are added and removed from only
+//   one end, called the "top" of the stack
+
+//  Operations on a stack
+//  1) push()
+//   Add element to the top of the stack
+//   psuedocode idea
+//   stack.push(12)
+
+
+//  2) pop()
+//   Removes element from the top of the stack
+//   psuedocode idea
+//   stack.pop()
+
+
+//  3) top()/peek()
+//   Returns the top/first element without removing it
+//   psuedocode idea
+//   stack.top()
+
+
+//  4) empty()
+//   Check whether  stack is empty or not
+//   or to check stack underflow
+//   psuedocode idea
+//    stack.empty()
+
+
+//   returns true if stack is empty else false
